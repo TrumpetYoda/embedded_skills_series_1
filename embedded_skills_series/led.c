4c5195e999a3dad9bc1c89b4e260f59819aae285
Alex Da Vera Cruz <jacrisspi77@gmail.com>
2023-09-09 16:56:00 -0400

    fixed blinking LEDs

--- embedded_skills_series/led.c
+++ embedded_skills_series/led.c
@@ -6,6 +6,8 @@ void init_led()
 {
     P1DIR |= red_LED; // direct pin as output
     P9DIR |= green_LED; // direct pin as output
-    P1OUT ___ red_LED; // turn red LED Off
-    P9OUT __ green_LED; // turn green LED on
+    P1OUT &= ~red_LED; // turn red LED Off
+    // P1OUT == 10101011 -> 10101011 & ~(00000001) == 10101010
+    P9OUT |= ~green_LED; // turn green LED on
+
 }
